babee0ae7d3dfdb607b5e92390bfd950e021e551
Antonio Jesús Ponce Vela <antoniojesuspv99@gmail.com>
2022-12-22 23:04:26 +0100

    implementación socket

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sockets/pages/home.dart';
+import 'package:flutter_sockets/pages/status.dart';
+import 'package:flutter_sockets/services/socket_service.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,11 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: 'home',
-      routes: {'home': (_) => HomePage()},
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => SocketService())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'status',
+        routes: {'home': (_) => HomePage(), 'status': (_) => StatusPage()},
+      ),
     );
   }
 }
